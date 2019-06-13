3ee7e20bbb899a09544d911297503cd60904afe0
gtgalone <sjhgenius@gmail.com>
2019-06-12 20:27:39 -0400

    Add first commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import { Link, Router } from '../routes'
+
+export default () => (
+  <ul>
+    <li>
+      <Link route='blog' params={{ slug: 'hello-world' }}>
+        <a>Blog: Hello world</a>
+      </Link>
+    </li>
+    <li>
+      <Link route='blog' params={{ slug: 'another-blog-post' }}>
+        <a>Blog: Another blog post</a>
+      </Link>
+    </li>
+    <li>
+      <Link route='blog' params={{ slug: 'non-existant' }}>
+        <a>Blog: Not found</a>
+      </Link>
+    </li>
+    <li>
+      <button onClick={() => Router.pushRoute('about', { foo: 'bar' })}>
+        About foo bar
+      </button>
+    </li>
+    <li>
+      <button onClick={() => Router.pushRoute('about', { foo: 'baz' })}>
+        About foo baz
+      </button>
+    </li>
+    <li>
+      <Link route='hello'>
+        <a>hello</a>
+      </Link>
+    </li>
+  </ul>
+)
---
+++ routes.js
@@ -0,0 +1,6 @@
+const nextRoutes = require('next-routes')
+const routes = (module.exports = nextRoutes())
+
+routes.add('blog', '/blog/:slug')
+routes.add('about', '/about-us/:foo(bar|baz)')
+routes.add('hello', '/hello')
